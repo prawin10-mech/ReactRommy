b7d0e3589b28ff70cfac88fbc5c9b6d9fc0c86ce
prawin10-mech <pra6650@gmail.com>
2023-05-19 11:33:59 +0530

    updated the nav

--- src/components/UI/Nav.js
+++ src/components/UI/Nav.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import RoomyFinderLogo from "../../assets/roomyFinderLogo.jpg.png";
-import { NavLink, Link, useNavigate } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import {
   Button,
   Stack,
