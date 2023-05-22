87ec64d928d687ec42c72e58a3c806a909cfdfc9
prawin10-mech <pra6650@gmail.com>
2023-05-22 13:04:31 +0530

    updated the my bookings

--- src/pages/MyBookings.js
+++ src/pages/MyBookings.js
@@ -7,6 +7,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { UserActions } from "../store/User";
 import DummyImage from "../assets/demo.jpg";
 import { useNavigate } from "react-router-dom";
+import Cookie from "js-cookie";
 
 const MyBookings = () => {
   const dispatch = useDispatch();
@@ -16,14 +17,11 @@ const MyBookings = () => {
 
   const fetchMyBookings = async () => {
     try {
-      // const { data } = await axios.get(
-      //   "https://roomy-finder-evennode.ap-1.evennode.com/api/v1/bookings/property-ad",
-      //   { headers: { Authorization: token } }
-      // );
-
-      const { data } = await axios.post(
-        "https://roomy-finder-evennode.ap-1.evennode.com/api/v1/ads/property-ad/available",
-        { countryCode: "AE" }
+      const user = JSON.parse(Cookie.get("user"));
+      console.log(user.type);
+      const { data } = await axios.get(
+        "https://roomy-finder-evennode.ap-1.evennode.com/api/v1/bookings/property-ad",
+        { headers: { Authorization: token } }
       );
       console.log(data);
 
@@ -33,37 +31,100 @@ const MyBookings = () => {
     }
   };
 
+  const capitalize = (str) => {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  };
+
+  const getStatusColor = (status) => {
+    if (status === "offered") {
+      return "green";
+    } else if (status === "pending") {
+      return "blue";
+    }
+    return "inherit";
+  };
+
+  const formatDateTime = (dateTimeString) => {
+    const options = {
+      weekday: "short",
+      month: "short",
+      day: "numeric",
+      year: "numeric",
+      hour: "numeric",
+      minute: "2-digit",
+      hour12: true,
+    };
+    const dateTime = new Date(dateTimeString);
+    return `${dateTime.toLocaleDateString(undefined, options)}`;
+  };
+
   useEffect(() => {
     fetchMyBookings();
   }, []);
 
   const myBookingData = myBookings?.map((booking) => (
-    <Grid key={booking._id} item xs={12} sm={6} md={4} lg={3}>
+    <Grid
+      key={booking._id}
+      item
+      xs={12}
+      sm={6}
+      md={4}
+      lg={3}
+      sx={{ cursor: "pointer", p: 2 }}
+      onClick={() => navigate(`/myBookings/aboutBooking/${booking.id}`)}
+    >
       <Grid
         sx={{
           width: "300px",
-          height: "250px",
+          backgroundColor: "#f5f5f5",
+          border: "1px solid #ccc",
+          borderRadius: "10px",
         }}
       >
         <CardMedia
           component="img"
           sx={{
-            // objectFit: "cover",
             width: "100%",
             height: "200px",
             padding: "10px",
-            borderRadius: "20px",
-            display: "flex",
-            cursor: "pointer",
+            borderRadius: "10px 10px 0 0",
+            objectFit: "cover",
           }}
-          onClick={() => navigate(`/rooms/view-room/${booking.id}`)}
-          image={booking.images.length > 0 ? booking.images[0] : [DummyImage]}
+          image={
+            booking.ad.images.length > 0 ? booking.ad.images[0] : [DummyImage]
+          }
           alt={booking?.id}
         />
         <Grid sx={{ padding: "10px" }}>
-          <Typography variant="subtitle1">{booking.type}</Typography>
           <Typography variant="subtitle1">
-            {booking.monthlyPrice} AED
+            Property:{" "}
+            <Typography component="span" sx={{ fontWeight: "700" }}>
+              {booking.quantity} {booking.ad.type}
+            </Typography>
+          </Typography>
+          <Typography variant="subtitle1">
+            Location:{" "}
+            <Typography component="span" sx={{ fontWeight: "700" }}>
+              {booking.ad.address.buildingName}
+            </Typography>
+          </Typography>
+          <Typography variant="subtitle1">
+            Status:{" "}
+            <Typography
+              component="span"
+              sx={{
+                fontWeight: "700",
+                color: getStatusColor(booking?.status),
+              }}
+            >
+              {!booking?.isPayed ? capitalize(booking.status) : "Paid"}
+            </Typography>
+          </Typography>
+          <Typography variant="subtitle1">
+            Date:{" "}
+            <Typography component="span" sx={{ fontWeight: "700" }}>
+              {formatDateTime(new Date(booking.createdAt).toISOString())}
+            </Typography>
           </Typography>
         </Grid>
       </Grid>
@@ -80,7 +141,8 @@ const MyBookings = () => {
         spacing={3}
         justifyContent="center"
         alignItems="center"
-        sx={{ margin: "auto", maxWidth: 1200 }}
+        gap={4}
+        sx={{ margin: "auto", maxWidth: 1200, mb: 5 }}
       >
         {myBookingData}
       </Grid>
